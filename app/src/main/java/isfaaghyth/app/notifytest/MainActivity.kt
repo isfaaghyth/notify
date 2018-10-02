cacdbd1584e74ce0f49f78c5e3bca1b86e88bc68
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-10-03 03:45:39 +0700

    refactor some code

--- app/src/main/java/isfaaghyth/app/notifytest/MainActivity.kt
+++ app/src/main/java/isfaaghyth/app/notifytest/MainActivity.kt
@@ -15,28 +15,25 @@ class MainActivity : AppCompatActivity() {
 
     private var composite: CompositeDisposable = CompositeDisposable()
     private lateinit var textView: TextView
-    private lateinit var listen: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.txtHelo)
-        Notify.send(Person("Muh Isfhani", 21))
         getNotify()
+        textView.setOnClickListener {
+            Notify.send(Person("Muh Isfhani", 21))
+        }
     }
 
     private fun getNotify() {
-        listen = Notify.listen(Person::class.java, NotifyProvider(), Consumer {
+        composite.add(Notify.listen(Person::class.java, NotifyProvider(), Consumer {
             person -> textView.text = person.name
-        })
-
-        composite.add(listen)
+        }))
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Notify.unlisten(listen)
-
         //or with...
         composite.clear()
     }
